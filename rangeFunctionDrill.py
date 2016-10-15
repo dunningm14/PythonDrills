6fcc8466b620b3aa067aff691da8f867468d6866
dunningm14 <dunningm14@georgefox.edu>
2016-10-14 21:20:11 -0500

    Add files via upload

---
+++ rangeFunctionDrill.py
@@ -0,0 +1,25 @@
+# Python 2.7.12
+#
+# Author: Madison Dunning
+#
+# Purpose: Range function drill
+
+
+# range() function with one parameter to make [0, 1, 2, 3] print
+
+myList = range(4)
+print myList
+
+
+
+# use the range() function with 3 parameters to make [3, 2, 1, 0] print
+
+myList2 = range(3, -1, -1)
+print myList2
+
+
+
+# use the range() function with 3 parameters to make [8, 6, 4, 2] print
+
+myList3 = range(8, 1, -2)
+print myList3

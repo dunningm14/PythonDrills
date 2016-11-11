02330d589adc4378fa39fa9fd8dfe4ef413b5db4
dunningm14 <dunningm14@georgefox.edu>
2016-11-11 17:50:22 -0600

    Update pyDrill_GUI.py

--- pyDrill_GUI.py
+++ pyDrill_GUI.py
@@ -7,14 +7,19 @@
 
 # Do Tkinter imports
 from Tkinter import *
+from tkFileDialog import *
 import ttk
+import tkMessageBox
+
 
 # Do imports for the code
-from datetime import datetime, timedelta
+from datetime import datetime
+from datetime import timedelta
 import os
 import time
 import shutil
-from glob import glob
+import datetime as dt
+import glob
 
 
 class FileCheck:
@@ -37,8 +42,7 @@ class FileCheck:
         self.dest = (self.destFolderName.get())
         
         # Title
-        headerLabel = ttk.Label(self.frame_header, image = self.logo).grid(row = 0, column = 0, columnspan = 2, pady = 5, sticky = 'w')
-        titleLabel = ttk.Label(self.frame_header, text = 'File Transfer Drill ')
+        titleLabel = ttk.Label(self.frame_header, text = 'File Transfer Window ')
         titleLabel.grid(row = 0, column = 0, columnspan = 2, pady = 5, sticky = 'w')
 
         # Set up the frame for the labels and buttons
@@ -68,44 +72,49 @@ class FileCheck:
     # Window used as a browser for the files in the 'daily' folder
     def selectDailyFolder(self):
         # open a window in the 'A' Folder
-        self.dailyFileCheck = filedialog.askdirectory(initialdir = "C:\Users\Dunning\Desktop\A", title = "Select the folder to be checked daily") 
+        self.dailyFileCheck = askdirectory(initialdir = "C:\Users\Dunning\Desktop\A", title = "Select the folder to be checked daily") 
         self.dailyFolderName.set(self.dailyFileCheck)
         print (self.dailyFileCheck)
         print (self.dailyFolderName.get())
         
-# Window used as a browser for the files in the folder to contain copies        
+    # Window used as a browser for the files in the folder to contain copies        
     def selectDestFolder(self):
         # open a window in the 'B' Folder
-        self.destFileCheck = filedialog.askdirectory(initialdir = "C:\Users\Dunning\Desktop\B", title = "Select folder to contain copies") 
+        self.destFileCheck = askdirectory(initialdir = "C:\Users\Dunning\Desktop\B", title = "Select folder to contain copies") 
         self.destFolderName.set(self.destFileCheck)
         print (self.destFileCheck)
         print (self.destFolderName.get())
 
+        
 
 # Script to automate the task of copying files to a specific location (folder B)
 # from their source (folder A) if the files have been created/edited within the past 24
-# hours.                    
-def copyFile(src, dst):
-    # sets current time to a variable
-    now = dt.datetime.now()
-    # sets the time 24 hours ago to a variable
-    ago = now-dt.timedelta(minutes=1440)
+# hours.
 
-    # looks in the source folder which was passed in
-    for file in os.listdir(src):
-         # creates new variable with the full path name to the file
-         full_path = os.path.join(src, file)
-         # sets time stamp to each file to the mtime variable
-         st = os.stat(full_path)
-         mtime = dt.datetime.fromtimestamp(st.st_mtime)
-         # checks to see which files have been edited in the last day
-         if mtime > ago:
-             # checks to make sure only .txt files are being copied
-             if file.lower().endswith(('.txt')):    
-                 shutil.copy(full_path, dst)
-                 print('%s modified %s'%(src +file, mtime))
+    def timeCompare(self, dailyFileCheck, destFileCheck):
+        # sets current time to a variable
+        now = dt.datetime.now()
+        # sets the time 24 hours ago to a variable
+        ago = now-dt.timedelta(minutes=1440)
 
-copyFile("C:\Users\Dunning\Desktop\A", "C:\Users\Dunning\Desktop\B")                    
+        # looks in the source folder which was passed in
+        for file in os.listdir(dailyFileCheck):
+             # creates new variable with the full path name to the file
+             full_path = os.path.join(dailyFileCheck, file)
+             # sets time stamp to each file to the mtime variable
+             st = os.stat(full_path)
+             mtime = dt.datetime.fromtimestamp(st.st_mtime)
+             # checks to see which files have been edited in the last day
+             if mtime > ago:
+                 # checks to make sure only .txt files are being copied
+                 if file.lower().endswith(('.txt')):    
+                     shutil.copy(full_path, destFileCheck)
+                     print('%s modified %s'%(dailyFileCheck +file, mtime)) 
+             if mtime > ago:
+                     print (full_path, "copied to: ", destFileCheck)
+                     shutil.copy(full_path,destFileCheck) #copys file to destination
+             else:
+                     print (full_path, 'not copied')
 
 def main():
     root = Tk() 
